93e16889d5003c47fb8d3ad3978f79033e185bf0
EugeneMaslov <zheka.maslov.18@gmail.com>
2025-05-12 23:46:43 +0300

    Реализовано чтение данных из файла

--- cpp-console-library-subscription/cpp-console-library-subscription/cpp-console-library-subscription.cpp
+++ cpp-console-library-subscription/cpp-console-library-subscription/cpp-console-library-subscription.cpp
@@ -1,21 +1,47 @@
-﻿// cpp-console-library-subscription.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-// ВАРИАНТ 7 - Банковские операции
+﻿// ВАРИАНТ 7 - Банковские операции
 // Маслов Евгений Александрович
 
 #include <iostream>
 
+using namespace std;
+
+#include "bank_operations.h"
+#include "file_reader.h"
+#include "constants.h"
+
 int main()
 {
-    std::cout << "Заготовка проекта\n";
+    setlocale(LC_ALL, "Russian");
+    cout << "Лабораторная работа №1. GIT\n";
+    cout << "Вариант №7. Банковские операции\n";
+    cout << "Автор: Маслов Евгений\n\n";
+    bank_operation* operations[MAX_FILE_ROWS_COUNT];
+    int size;
+    try
+    {
+        read("data.txt", operations, size);
+        for (int i = 0; i < size; i++)
+        {
+            cout << operations[i]->date.day << '.';
+            cout << operations[i]->date.month << '.';
+            cout << operations[i]->date.year << '\n';
+            cout << operations[i]->time.day << ':';
+            cout << operations[i]->time.month << ':';
+            cout << operations[i]->time.year << '\n';
+            cout << operations[i]->account.form << '\n';
+            cout << operations[i]->account.inv << '\n';
+            cout << operations[i]->sum << '\n';
+            cout << operations[i]->aim << '\n';
+            cout << '\n';
+        }
+        for (int i = 0; i < size; i++)
+        {
+            delete operations[i];
+        }
+    }
+    catch (const char* error)
+    {
+        cout << error << '\n';
+    }
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
