c4d4d6ec857289a9b848b86b033c92fda95328d4
EugeneMaslov <zheka.maslov.18@gmail.com>
2025-05-17 18:04:22 +0300

    Добавлены unit-тесты

---
+++ cpp-console-library-subscription/cpp-console-library-subscription/process.cpp
@@ -0,0 +1,6 @@
+#include "process.h"
+
+int process(bank_operation* array[], int size)
+{
+	return 0;
+}
---
+++ cpp-console-library-subscription/cpp-console-library-subscription/process.h
@@ -0,0 +1,8 @@
+#ifndef PROCESS_H
+#define PROCESS_H
+
+#include "bank_operations.h"
+
+int process(bank_operation* array[], int size);
+
+#endif
